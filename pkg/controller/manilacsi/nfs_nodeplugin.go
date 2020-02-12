7930488c250a60444f058334842f93f21c57bb7b
Mike Fedosin <mfedosin@redhat.com>
2020-02-12 19:27:15 +0100

    Convert nfs plugin yaml to golang struct

--- pkg/controller/manilacsi/nfs_nodeplugin.go
+++ pkg/controller/manilacsi/nfs_nodeplugin.go
@@ -1,77 +1,22 @@
 package manilacsi
 
 import (
-	"bytes"
 	"context"
 
 	manilacsiv1alpha1 "github.com/Fedosin/csi-driver-manila-operator/pkg/apis/manilacsi/v1alpha1"
 	"github.com/go-logr/logr"
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	k8sYaml "k8s.io/apimachinery/pkg/util/yaml"
-)
-
-var (
-	nfsNodePluginManifest = `kind: DaemonSet
-apiVersion: apps/v1
-metadata:
-  name: csi-nodeplugin-nfsplugin
-  namespace: default
-spec:
-  selector:
-    matchLabels:
-      app: csi-nodeplugin-nfsplugin
-  template:
-    metadata:
-      labels:
-        app: csi-nodeplugin-nfsplugin
-    spec:
-      containers:
-        - name: nfs
-          securityContext:
-            privileged: true
-            capabilities:
-              add: ["SYS_ADMIN"]
-            allowPrivilegeEscalation: true
-          image: quay.io/k8scsi/nfsplugin:canary
-          args:
-            - "--nodeid=$(NODE_ID)"
-            - "--endpoint=unix://plugin/csi.sock"
-          env:
-            - name: NODE_ID
-              valueFrom:
-                fieldRef:
-                  fieldPath: spec.nodeName
-          imagePullPolicy: IfNotPresent
-          volumeMounts:
-            - name: plugin-dir
-              mountPath: /plugin
-            - name: pods-mount-dir
-              mountPath: /var/lib/kubelet/pods
-              mountPropagation: Bidirectional
-      volumes:
-        - name: plugin-dir
-          hostPath:
-            path: /var/lib/kubelet/plugins/csi-nfsplugin
-            type: DirectoryOrCreate
-        - name: pods-mount-dir
-          hostPath:
-            path: /var/lib/kubelet/pods
-            type: Directory
-`
 )
 
 func (r *ReconcileManilaCSI) handleNFSNodePluginDaemonSet(instance *manilacsiv1alpha1.ManilaCSI, reqLogger logr.Logger) error {
 	reqLogger.Info("Reconciling NFS Node Plugin DaemonSet")
 
 	// Define a new DaemonSet object
-	ds := &appsv1.DaemonSet{}
-
-	dec := k8sYaml.NewYAMLOrJSONDecoder(bytes.NewReader([]byte(nfsNodePluginManifest)), 1000)
-	if err := dec.Decode(&ds); err != nil {
-		return err
-	}
+	ds := generateNFSNodePluginManifest()
 
 	// Check if this DaemonSet already exists
 	found := &appsv1.DaemonSet{}
@@ -92,4 +37,101 @@ func (r *ReconcileManilaCSI) handleNFSNodePluginDaemonSet(instance *manilacsiv1a
 	// DaemonSet already exists - don't requeue
 	reqLogger.Info("Skip reconcile: DaemonSet already exists", "DaemonSet.Namespace", found.Namespace, "DaemonSet.Name", found.Name)
 	return nil
+}
+
+func generateNFSNodePluginManifest() *appsv1.DaemonSet {
+	trueVar := true
+
+	hostPathDirectoryOrCreate := corev1.HostPathDirectoryOrCreate
+	hostPathDirectory := corev1.HostPathDirectory
+
+	mountPropagationBidirectional := corev1.MountPropagationBidirectional
+
+	labels := map[string]string{
+		"app": "csi-nodeplugin-nfsplugin",
+	}
+
+	return &appsv1.DaemonSet{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "DaemonSet",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "csi-nodeplugin-nfsplugin",
+			Namespace: "default",
+		},
+		Spec: appsv1.DaemonSetSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "nfs",
+							Image: "quay.io/k8scsi/nfsplugin:canary",
+							Args: []string{
+								"--nodeid=$(NODE_ID)",
+								"--endpoint=unix://plugin/csi.sock",
+							},
+							Env: []corev1.EnvVar{
+								{
+									Name: "NODE_ID",
+									ValueFrom: &corev1.EnvVarSource{
+										FieldRef: &corev1.ObjectFieldSelector{
+											FieldPath: "spec.nodeName",
+										},
+									},
+								},
+							},
+							SecurityContext: &corev1.SecurityContext{
+								Privileged:               &trueVar,
+								AllowPrivilegeEscalation: &trueVar,
+								Capabilities: &corev1.Capabilities{
+									Add: []corev1.Capability{
+										"SYS_ADMIN",
+									},
+								},
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "plugin-dir",
+									MountPath: "/plugin",
+								},
+								{
+									Name:             "pods-mount-dir",
+									MountPath:        "/var/lib/kubelet/pods",
+									MountPropagation: &mountPropagationBidirectional,
+								},
+							},
+							ImagePullPolicy: "IfNotPresent",
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "plugin-dir",
+							VolumeSource: corev1.VolumeSource{
+								HostPath: &corev1.HostPathVolumeSource{
+									Path: "/var/lib/kubelet/plugins/csi-nfsplugin",
+									Type: &hostPathDirectoryOrCreate,
+								},
+							},
+						},
+						{
+							Name: "pods-mount-dir",
+							VolumeSource: corev1.VolumeSource{
+								HostPath: &corev1.HostPathVolumeSource{
+									Path: "/var/lib/kubelet/pods",
+									Type: &hostPathDirectory,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
 }
