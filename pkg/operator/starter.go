fba2cba153cb5b8d0a017ba0ef225050668f4f78
Stephen Finucane <stephenfin@redhat.com>
2023-07-06 14:45:29 +0100

    OCPBUGS-14049: Don't cache OpenStack client
    We are initialising an OpenStack client and storing it as an attribute
    of ManilaController. However, this takes the credentials at the time of
    client creation. These credentials can change over time, as a result of
    e.g. password rotation.
    
    We opt to avoid caching the client, creating it each time we need it
    rather than once during creation of the controller instance. Given the
    sync interval, the overhead of this should be minimal.
    
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -198,11 +198,6 @@ func RunOperator(ctx context.Context, controllerConfig *controllercmd.Controller
 		return err
 	}
 
-	openstackClient, err := manila.NewOpenStackClient(util.CloudConfigFilename, kubeInformersForNamespaces)
-	if err != nil {
-		return err
-	}
-
 	secretSyncController := secret.NewSecretSyncController(
 		operatorClient,
 		kubeClient,
@@ -214,7 +209,6 @@ func RunOperator(ctx context.Context, controllerConfig *controllercmd.Controller
 		operatorClient,
 		kubeClient,
 		kubeInformersForNamespaces,
-		openstackClient,
 		[]manila.Runnable{
 			csiDriverControllerSet,
 			nfsCSIDriverController,
